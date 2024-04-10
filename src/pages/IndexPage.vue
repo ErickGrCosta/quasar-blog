14037e443007d33ea21b295719cdf5abd2b98353
ErickGrCosta <erickgrcosta@gmail.com>
2024-04-10 20:06:00 -0300

    Criação e compreensão do funcionamento de cada uma das funções do vuex

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,11 +2,14 @@
   <q-page class="flex flex-center">
     <button @click="getItens">Get</button>
     <button @click="createItens">Post</button>
+    <button @click="updateItem('b646')">Update</button>
+    <button @click="deleteItem('38b2')">Delete</button>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    {{ totalOfAuthors  }}
   </q-page>
 </template>
 
@@ -15,28 +18,59 @@ defineOptions({
   name: 'IndexPage'
 })
 
-import { api } from 'src/boot/axios'
-import { mapActions, mapState } from 'vuex'
+import { useStore } from 'vuex'
+import { onMounted, computed, reactive } from 'vue'
 
-function getItens () {
-  api.get('authors')
-    .then(res => {
-      console.log('Deu certo graças a Deus!', res.data)
-    })
-    .catch(error => {
-      console.error('Deu ruuuuuim!', error)
-    })
+const state = reactive({
+  authors: ''
+})
+
+const store = useStore()
+
+const totalOfAuthors = computed(() => {
+  return store.state.authors
+})
+
+onMounted(() => getItens())
+
+async function getItens () {
+  try {
+    console.log(store)
+    await store.dispatch('authors/fetchAuthors')
+    state.authors = store.state.authors
+  } catch (error) {
+    console.error('Error fetching data: ', error)
+  }
 }
 
-function createItens () {
-  const postData = { post: 'hey there' }
-
-  api.post('posts', postData)
-    .then(res => {
-      console.log('Deu bom!', res.data)
-    })
-    .catch(error => {
-      console.error('Deu ruuuuuim!', error)
-    })
+async function createItens () {
+  try {
+    await store.dispatch('authors/createAuthor', { newTest: 'teste2' })
+  } catch (error) {
+    console.error('Error fetching data: ', error)
+  }
 }
+
+async function updateItem (id) {
+  try {
+    await store.dispatch('authors/updateAuthor', {
+      authorId: 'b646',
+      payload: {
+        newTest: 'Mudei de novo',
+        newItem: 'Mudei'
+      }
+    })
+  } catch (error) {
+    console.error('Erro:', error)
+  }
+}
+
+async function deleteItem (id) {
+  try {
+    await store.dispatch('authors/deleteAuthor', id)
+  } catch (error) {
+    console.error('Erro:', error)
+  }
+}
+
 </script>
